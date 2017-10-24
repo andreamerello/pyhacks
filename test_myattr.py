0bb9c5e76c493cb79f4be69369f109f7db0deecc
Antonio Cuni <anto.cuni@gmail.com>
2017-10-24 23:55:52 +0200

    more comments

--- test_myattr.py
+++ test_myattr.py
@@ -25,12 +25,19 @@ class TestMyattr:
         foo2 = my_attr.s(foo)
         assert foo is foo2
 
+    # general rule: test names should be self descriptive. Here, it's not
+    # really clear what are you testing; "add" what? For an example of
+    # long&descriptive name :)
+    # https://bitbucket.org/pypy/pypy/src/1c3af3c4a0958c4cf5b1197418659829d73a06d2/rpython/jit/metainterp/optimizeopt/test/test_optimizebasic.py?at=default&fileviewer=file-view-default#test_optimizebasic.py-598
     def test_add(self):
         @my_attr.s
         class foo(object):
             bar = my_attr.ib()
 
         A = foo(None)
+        # what are you testing here? If you are testing that the __init__
+        # works correctly, it is basically the same test as test_val: "assert
+        # A.bar is None" is better than this assert here
         assert 'bar' in foo.__dict__
 
     def test_val(self):
