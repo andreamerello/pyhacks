15881c9497208551bcad9996d786ac7c79a84f45
Andrea Merello <andrea.merello@gmail.com>
2017-10-24 20:24:15 +0200

    generate __init__ segnature
    Signed-off-by: Andrea Merello <andrea.merello@gmail.com>

--- my_attr.py
+++ my_attr.py
@@ -5,16 +5,26 @@ class foowrap(object):
         self.val = val
 
 class my_attr:
-    inits = []
     @staticmethod
     def s(cls_deco):
-        def magic_wand(self):
-            for attr in dir(self):
-                obj = getattr(self, attr)
-                if type(obj) is foowrap:
-                    setattr(self, attr, obj.val)
         def put_init():
+            coma = ''
+            sign_str = ''
+            assign_str = ''
+            for attr in dir(cls_deco):
+                obj = getattr(cls_deco, attr)
+                if type(obj) is foowrap:
+                    sign_str += ', ' + attr
+                    if obj.val is not None:
+                        sign_str += "=" + str(obj.val) # is str() ok ?
+                    assign_str += '    self.' + attr + ' = ' + attr + '\n'
+            if assign_str == '':
+                assign_str = '    pass'
+            magic_str = 'def magic_wand(self' + sign_str + '):\n' + assign_str
+            print magic_str
+            exec magic_str in globals(), locals()
             cls_deco.__init__ = magic_wand
+
             return cls_deco
 
         return put_init()
@@ -22,3 +32,10 @@ class my_attr:
     @staticmethod
     def ib(val = None):
         return foowrap(val)
+
+
+@my_attr.s
+class foo(object):
+    bar = my_attr.ib(5)
+
+A = foo(5)
--- test_myattr.py
+++ test_myattr.py
@@ -15,14 +15,21 @@ class TestMyattr:
         class foo(object):
             bar = my_attr.ib()
 
-        A = foo()
+        A = foo(None)
         assert 'bar' in foo.__dict__
-
 
     def test_val(self):
         @my_attr.s
         class foo(object):
-            bar = my_attr.ib(5)
+            bar = my_attr.ib()
+
+        A = foo(bar = 5)
+        assert A.bar == 5
+
+    def test_def_val(self):
+        @my_attr.s
+        class foo(object):
+            bar = my_attr.ib(3)
 
         A = foo()
-        assert A.bar == 5
+        assert A.bar == 3
