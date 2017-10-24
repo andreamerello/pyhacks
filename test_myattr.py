5f7c3d43876b34d1c4bc915c017c868f72448ea7
Antonio Cuni <anto.cuni@gmail.com>
2017-10-25 00:04:20 +0200

    another failing test

--- test_myattr.py
+++ test_myattr.py
@@ -55,3 +55,54 @@ class TestMyattr:
 
         A = foo()
         assert A.bar == 3
+
+    # xfail: eXpected to fail: the test is still run, but pytest still shows a
+    # green line. You should delete the xfails one by one, and fix them
+    @pytest.mark.xfail
+    def test_default_None(self):
+        @my_attr.s
+        class Foo(object):
+            bar = my_attr.ib(None)
+
+        f = Foo()
+        assert f.bar is None
+
+    @pytest.mark.xfail
+    def test_default_keyword(self):
+        @my_attr.s
+        class Foo(object):
+            bar = my_attr.ib(default=None)
+
+        f = Foo()
+        assert f.bar is None
+
+    @pytest.mark.xfail
+    def test_attribute_order(self):
+        # this is a difficult test to write, because the dictionary order is
+        # undefined, so it might happen that the attributes are in the correct
+        # order by chance, even if you don't do anything to sort them.
+        # Moreover, dir() returns attributes in alphabetical order.
+        #
+        # To minimize the probability to pass the test "by chance", we define
+        # two classes with the attributes in reverse order.
+        @my_attr.s
+        class Point1(object):
+            x = my_attr.ib()
+            y = my_attr.ib()
+            z = my_attr.ib()
+
+        p = Point1(1, 2, 3)
+        assert p.x == 1
+        assert p.y == 2
+        assert p.z == 3
+
+        @my_attr.s
+        class Point2(object):
+            z = my_attr.ib()
+            y = my_attr.ib()
+            x = my_attr.ib()
+
+        p = Point2(1, 2, 3)
+        assert p.z == 1
+        assert p.y == 2
+        assert p.x == 3
